3ebe7d06a40302147b3cec32e31d2242bced5181
Rahul Singh <rsingh@artemishealth.com>
2022-06-24 14:47:18 +0530

    Final commit to be there
    mid code of patches
    
    mid extra code os check
    
    Final code changes

--- pylot.py
+++ pylot.py
@@ -1,3 +1,5 @@
+import os
+
 def patches() -> list:
     """ Patches for Unit Tests """
     test_cases = [
@@ -19,6 +21,37 @@ def patches() -> list:
     ]
     return test_cases
 
+def run_patches_configuration() -> bool:
+    """
+    To identify if the given patch is ok
+    """
+
+    flag = False
+
+    patches = 'os_get_information'
+    if 'o' in patches.split():
+         flag = True
+
+    return flag
+
+def os_installation_check() -> bool:
+    flag_os = False
+
+    if os.get_max_size() > 100:
+        flag_os = True
+    
+    if 'ram' in os.os_get_information:
+        flag_os = True
+    
+    return flag
+
+def ubuntu_installation() -> bool:
+    flag = False
+
+    if os.identify == 'ubuntu':
+        flag = True
+    
+    return flag
 
 def test_results() -> list:
     """Test Results for Unit Tests."""
